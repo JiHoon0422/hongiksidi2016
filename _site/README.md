9ae3eb12d907f99ac4da275ae4434dec37b7aab3
JiHoon <ljh62044217@gmail.com>
2017-02-06 23:36:05 +0900

    Fin

--- _site/README.md
+++
@@ -1,3 +0,0 @@
-# hongiksidi2016
-Hongiksidi 2016 Graduation Page Jekyll Updating Example ver.0.01
-JiHoon.
---
+++ function.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.category').on('change', function(){
+      var category_list = [];
+      $('#filters :input:checked').each(function(){
+        var category = $(this).val();
+        category_list.push(category);
+      });
+
+      if(category_list.length == 0)
+        $('.resultblock').fadeIn();
+      else {
+        $('.resultblock').each(function(){
+          var item = $(this).attr('data-tag');
+          if(jQuery.inArray(item,category_list) > -1)
+            $(this).fadeIn('slow');
+          else
+            $ (this).hide();
+        });
+      }   
+    });
+    console.log("DefaultFilter")
+  }); 
